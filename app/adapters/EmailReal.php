e390ba49dddf06ca82c28a33fec404c058f70773
Richard McIntyre <richard.mackstar@gmail.com>
2012-10-06 12:20:58 +0100

    Added docblocks

--- app/adapters/EmailReal.php
+++ app/adapters/EmailReal.php
@@ -2,8 +2,17 @@
 
 namespace adapters;
 
+/**
+ * A real email class
+ *
+ */
 class EmailReal
 {
+	/**
+	 * I am a docblock
+	 *
+	 * @return void
+	 */
 	public function send() {
 		echo 'email real send';
 	}
--- app/adapters/EmailTest.php
+++ app/adapters/EmailTest.php
@@ -2,8 +2,17 @@
 
 namespace adapters;
 
+/**
+ * A fake email class
+ *
+ */
 class EmailTest
 {
+	/**
+	 * I am a docblock
+	 *
+	 * @return void
+	 */
 	public function send() {
 		echo 'email test send';
 	}
